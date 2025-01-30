95a00ae7c07b66b5d6339ddab85a530613a05f28
User <user@example.com>
2025-01-30 23:46:23 +0200

    test

---
+++ supabase/functions/openai/index.ts
@@ -0,0 +1,37 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { GoogleGenerativeAI } from "npm:@google/generative-ai";
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-exp-1206",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+Deno.serve(async (req) => {
+  const data = await req.json();
+  
+  const history = data.messages.map(message => ({
+    role: message.role,
+    parts: message.content.map(content => ({ text: content.text }))
+  }));
+
+  const chatSession = model.startChat({
+    generationConfig,
+    history,
+  });
+
+  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
+  return new Response(
+    JSON.stringify({ response: result.response.text() }),
+    { headers: { "Content-Type": "application/json" } },
+  );
+});
